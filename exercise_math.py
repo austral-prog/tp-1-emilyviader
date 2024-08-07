abdde03bb649cb0b7c910bcc1b6fd9d3c1142055
emilyviader <emily.viader@gmail.com>
2024-08-07 15:13:31 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,26 @@
 def math():
     a = 57
     b = 7
+    
+    suma = a + b
+    print(suma)
+    
+    dif = a - b
+    print(dif)
+    
+    prod = a * b
+    print(prod)
+    
+    avg = (a + b) / 2
+    print(avg)
+
+    c_entero = a//b
+    print(c_entero)
+        
+    resto_d_e =  a % b 
+    print(resto_d_e)
+
+    valor_real = a / b
+    print(valor_real)
+    
+math()
